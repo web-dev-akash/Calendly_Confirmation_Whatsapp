5bdbcb1524ebf6791a7cbd3a7a23b9e754cf8ad3
web-dev-akash <akumar415129.ak@gmail.com>
2023-08-22 17:47:36 +0530

    Final Commit

--- index.js
+++ index.js
@@ -16,19 +16,10 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
   try {
-    let msg = req.body;
-    msg = msg.split("\n");
-    const [foundPhone] = msg.filter((val) => val.includes("Phone No"));
-    let phone = foundPhone.substring(11, foundPhone.length);
-    const [foundEmail] = msg.filter((val) => val.includes("Invitee Email"));
-    let email = foundEmail.substring(15, foundEmail.length);
-    email = email.trim();
+    let phone = req.body;
     const newPhone = phone.replace(/\s/g, "");
-    console.log(foundPhone);
-    console.log(foundEmail);
     return res.status(200).send({
       phone: newPhone,
-      email: email,
     });
   } catch (error) {
     return res.status(500).send({ error });
